7c844165e433742088114735912c1c4bfdae1cbe
anastasia <nastya16.com@gmail.com>
2023-04-18 20:11:01 +0300

    first lesson complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import './App.css';
 import {TaskType, TodoList} from "./TodoList";
 
 function App() {
-    let tasks1: Array<TaskType>=[
+    let tasks: Array<TaskType>=[
         {id:1, title: "CSS", isDone: true},
         {id:2, title: "JS", isDone: true},
         {id:3, title: "React", isDone: false}
     ];
-    let tasks2: Array<TaskType>=[
+  /*  let tasks2: Array<TaskType>=[
         {id:1, title: "Terminator", isDone: false},
         {id:2, title: "Demon slayer", isDone: true},
         {id:3, title: "Dogma", isDone: true}
-    ];
+    ];*/
+    function removeTask(id: number){
+        let resultTasks=tasks.filter(()=> {return true});
+    }
     return (
         <div className="App">
-            <TodoList title={"What to learn"} tasks={tasks1}/>
-            <TodoList title={"Movies"} tasks={tasks2}/>
+            <TodoList title={"What to learn"} tasks={tasks}/>
+
 
         </div>
     );
