df92d11dacef222ff27cbf4c1122798779ff197a
anastasia <nastya16.com@gmail.com>
2023-04-18 14:03:23 +0300

    first lesson complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
+import {TaskType, TodoList} from "./TodoList";
 
 function App() {
+    let tasks1: Array<TaskType>=[
+        {id:1, title: "CSS", isDone: true},
+        {id:2, title: "JS", isDone: true},
+        {id:3, title: "React", isDone: false}
+    ];
+    let tasks2: Array<TaskType>=[
+        {id:1, title: "Terminator", isDone: false},
+        {id:2, title: "Demon slayer", isDone: true},
+        {id:3, title: "Dogma", isDone: true}
+    ];
     return (
         <div className="App">
-            <TodoList/>
-            <TodoList/>
-            <TodoList/>
+            <TodoList title={"What to learn"} tasks={tasks1}/>
+            <TodoList title={"Movies"} tasks={tasks2}/>
+
         </div>
     );
 }
